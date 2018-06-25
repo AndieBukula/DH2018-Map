7bcb5771896f8088c0e7a8bf6d68e0b887a1088e
AndieBukula <40573426+AndieBukula@users.noreply.github.com>
2018-06-25 17:53:30 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nVK_XiG7xoAAqorxOpYW258Gx7zP2jns4oP_fsbx3QA/edit#gid=0';
